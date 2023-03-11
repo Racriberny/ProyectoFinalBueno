f33f410ac153f498e0d0bf673fa30cfbf7034e0d
Cristobal <cristobaljunior9@gmail.com>
2023-03-11 02:09:44 +0100

    Initial commit

---
+++ app/src/main/java/com/cristobalbernal/proyectofinal/ui/home/HomeFragment.java
@@ -0,0 +1,40 @@
+package com.cristobalbernal.proyectofinal.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.cristobalbernal.proyectofinal.R;
+import com.cristobalbernal.proyectofinal.databinding.FragmentHomeBinding;
+import com.cristobalbernal.proyectofinal.ui.gallery.GalleryFragment;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding.bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return binding.getRoot();
+    }
+    //Preguntar German que hace esto!!!
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
