29a5addf9ae5b49bdc298feeed0c9c31cbbc32a1
Cristobal <cristobaljunior9@gmail.com>
2023-03-14 18:01:58 +0100

    funciona!!

--- app/src/main/java/com/cristobalbernal/proyectofinal/MainActivity.java
+++ app/src/main/java/com/cristobalbernal/proyectofinal/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         //Aqui es donde se pone la funcion de ir!!
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_login,R.id.nav_register)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
--- app/src/main/java/com/cristobalbernal/proyectofinal/ui/home/HomeFragment.java
+++ app/src/main/java/com/cristobalbernal/proyectofinal/ui/home/HomeFragment.java
@@ -4,21 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.cristobalbernal.proyectofinal.R;
 import com.cristobalbernal.proyectofinal.databinding.FragmentHomeBinding;
-import com.cristobalbernal.proyectofinal.ui.gallery.GalleryFragment;
-
-import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
