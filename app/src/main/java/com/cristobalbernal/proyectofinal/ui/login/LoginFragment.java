e3b0ef606ce3f1f8ddbd3470d91344c43859e999
Cristobal <cristobaljunior9@gmail.com>
2023-03-14 18:32:25 +0100

    Funciona, por ahora los lenguajes estan bien!!!

---
+++ app/src/main/java/com/cristobalbernal/proyectofinal/ui/login/LoginFragment.java
@@ -0,0 +1,26 @@
+package com.cristobalbernal.proyectofinal.ui.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.cristobalbernal.proyectofinal.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+    private FragmentLoginBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/cristobalbernal/proyectofinal/ui/register/Register.java
@@ -0,0 +1,24 @@
+package com.cristobalbernal.proyectofinal.ui.register;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.cristobalbernal.proyectofinal.databinding.FragmentRegisterBinding;
+
+public class Register extends Fragment {
+    private FragmentRegisterBinding binding;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
