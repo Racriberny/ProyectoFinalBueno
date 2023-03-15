94a5ca3bd48d6e4832d7586224705f3a5bb4cf4a
Cristobal <cristobaljunior9@gmail.com>
2023-03-15 15:59:52 +0100

    Preguntar German!!!

---
+++ app/src/main/java/com/cristobalbernal/proyectofinal/ui/carta/CartaFragment.java
@@ -0,0 +1,12 @@
+package com.cristobalbernal.proyectofinal.ui.carta;
+import androidx.fragment.app.Fragment;
+import com.cristobalbernal.proyectofinal.R;
+
+public class CartaFragment extends Fragment {
+
+    public CartaFragment(){
+        super(R.layout.fragment_carta);
+    }
+
+
+}
